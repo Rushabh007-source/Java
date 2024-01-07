fd178fa5817deb62c5abf5809d8f8626df48dff2
Rushabh007-source <77576145+Rushabh007-source@users.noreply.github.com>
2024-01-07 12:08:37 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,17 @@
+package pack;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		// main function was written by writing 'main' then ctrl + space and then enter.
+		
+		System.out.println();
+		// the above line was written by typing 'syso' then ctrl + space.
+		
+	}
+	
+	
+	
+    
+
+}
